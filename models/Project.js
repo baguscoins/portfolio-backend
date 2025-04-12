f6bceed9fe55e105f3a6ee1c5ccf8eeee47cc310
baguscoins <freelancegus01@gmail.com>
2025-04-13 03:28:23 +0700

    Add files via upload

---
+++ models/Project.js
@@ -0,0 +1,9 @@
+
+const mongoose = require('mongoose');
+const ProjectSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  image: String,
+  link: String
+});
+module.exports = mongoose.model('Project', ProjectSchema);
